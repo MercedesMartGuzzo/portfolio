df7a750d8b62138a42079d27cfbbc044d500616d
Mercedes Martinez <mercedesviolin@gmail.com>
2025-03-05 17:21:35 -0300

    armando ek portfolio

---
+++ js/index.js
@@ -0,0 +1,49 @@
+/* loader */
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#spinner").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.querySelector("body").style.visibility = "visible";
+            document.querySelector("#spinner").style.visibility = "hidden";
+            document.querySelector("#spinner").style.opacity = "0";
+            document.querySelector("#spinner").style.transform = "translateY(100%)";
+        }, 400);
+        setTimeout(() => {
+            document.querySelector("#spinner").style.display = "none";
+        }, 2000);
+    }
+};
+
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const menuItems = document.querySelectorAll(".nav a");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    abrir.style.display = "none";
+    cerrar.style.display = "block";
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+
+    setTimeout(() => {
+        cerrar.style.display = "none";
+        abrir.style.display = "block";
+    }, 1000);
+});
+/* Cada vez q hago click en un iten la nav cierra */
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        nav.classList.remove("visible");
+
+        setTimeout(() => {
+            cerrar.style.display = "none";
+            abrir.style.display = "block";
+        }, 1000);
+    });
+});
